f4dad42ac4a5a2fb756e6a3f0eec2eb097a02a7f
Hashim Gari <hashmaster3k@gmail.com>
2020-07-22 14:23:12 -0600

    Adds ability to search and list all books

--- lib/library.rb
+++ lib/library.rb
@@ -1,10 +1,23 @@
+require './lib/author'
+
 class Library
 
-  attr_reader :name, :books, :authors
+  attr_reader :name, :authors
 
   def initialize(name)
     @name = name
-    @books = []
     @authors = []
+    @books = []
   end
+
+  def add_author(author)
+    @authors << author
+  end
+
+  def books
+    @authors.flat_map do |author|
+      author.books.each {|book| book}
+    end
+  end
+
 end
